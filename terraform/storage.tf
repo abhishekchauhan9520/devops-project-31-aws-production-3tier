87229e8314150b2d367b7409f05ec518710a86f0
abhishekchauhan9520 <63195310+abhishekchauhan9520@users.noreply.github.com>
2026-08-24 22:28:11 +0530

    feat: add private encrypted object storage

---
+++ terraform/storage.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "app" { bucket_prefix = "p31-app-" force_destroy = false }
+resource "aws_s3_bucket_public_access_block" "app" { bucket = aws_s3_bucket.app.id block_public_acls = true block_public_policy = true ignore_public_acls = true restrict_public_buckets = true }
+resource "aws_s3_bucket_versioning" "app" { bucket = aws_s3_bucket.app.id versioning_configuration { status = "Enabled" } }
+resource "aws_s3_bucket_server_side_encryption_configuration" "app" { bucket = aws_s3_bucket.app.id rule { apply_server_side_encryption_by_default { sse_algorithm = "AES256" } } }
